4ef563817859784e2be36fcb257e3e0243305dfc
Anton Ivanyutenko <anton.ivanyutenko@gmail.com>
2014-12-23 16:56:13 +0300

    Starting service

--- VozikGPS/src/by/aivanyutenko/vozikgps/MainActivity.java
+++ VozikGPS/src/by/aivanyutenko/vozikgps/MainActivity.java
@@ -1,6 +1,7 @@
 package by.aivanyutenko.vozikgps;
 
 import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,6 +12,8 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Intent intent = new Intent(this, GPSTrackerService.class);
+		startService(intent);
 	}
 
 	@Override
